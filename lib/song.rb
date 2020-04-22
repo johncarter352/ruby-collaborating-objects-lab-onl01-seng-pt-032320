1a947ff9036c888be9820db9ad119439bca65f97
John Carter <johncarter352@gmail.com>
2020-04-22 01:51:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,34 +1,14 @@
-class Song 
-  attr_accessor :name, :artist 
-  
-  @@all = []
-  
+class Song
+  attr_accessor :name, :artist
+
   def initialize(name)
-    @name = name 
-    save
+    @name = name
   end
-  
-  def save
-    @@all << self
-  end
-  
-  def self.all
-    @@all.inspect
-  end
-  
- def self.new_by_filename(filename)
+
+  def self.new_by_filename(filename)
     artist_name, song_name, extra = filename.split(" - ")
     song = self.new(song_name)
     artist = Artist.find_or_create_by_name(artist_name)
     artist.add_song(song)
   end
-  
-   def artist_name
-    if self.artist
-      self.artist.name
-    else
-      nil
-    end
-  end
-    
-  end
+end
