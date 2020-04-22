5ca50ba1647dce6971c331eecdaf8377a43608d7
John Carter <johncarter352@gmail.com>
2020-04-22 01:41:51 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+class Song 
+  attr_accessor :name, :artist 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @all
+  end
+  
+  def self.new_by_filename(file_name)
+    song = file_name.split(" - ")[1]
+    artist = file_name.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name = artist_name
+    new_song
+  end
+  
+   def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+    
+  end
