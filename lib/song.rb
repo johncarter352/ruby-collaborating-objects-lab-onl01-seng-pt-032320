3ef2d29e9b284358ae85ef2f36caaa6cb85ea80f
John Carter <johncarter352@gmail.com>
2020-04-22 01:50:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,12 +16,11 @@ class Song
     @@all.inspect
   end
   
-  def self.new_by_filename(file_name)
-    song = file_name.split(" - ")[1]
-    artist = file_name.split(" - ")[0]
-    new_song = self.new(song)
-    new_song.artist_name = artist_name
-    new_song
+ def self.new_by_filename(filename)
+    artist_name, song_name, extra = filename.split(" - ")
+    song = self.new(song_name)
+    artist = Artist.find_or_create_by_name(artist_name)
+    artist.add_song(song)
   end
   
    def artist_name
